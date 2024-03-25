9f2dda14781e66d81ffbf107df0db685d6920319
STIVEN HERNANDEZ <casper.sa690@gmail.com>
2024-03-25 10:14:27 -0500

    [Issue #1] create Student and Subject model

---
+++ Esperanza.Shared/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Universidad.Shared.Entities
+{
+    public class Student
+    {
+        private static int nextId = 1;
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<StudentSubject>? StudentSubjects { get; set; }
+        public double Average { get; set; }
+        public Student()
+        {
+            Id = nextId++;
+        }
+    }
+}
